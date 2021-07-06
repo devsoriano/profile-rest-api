860b03e42e513ffc85eac5ab869f9f1bd050cfbe
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2021-07-05 19:01:50 -0500

    cambios

--- projects_api/urls.py
+++ projects_api/urls.py
@@ -42,7 +42,7 @@ router.register('states', views.StatesViewSet)
 router.register('cities', views.CitiesViewSet)
 router.register('local-distances', views.LocalDistancesViewSet)
 router.register('airports', views.AirportsViewSet)
-router.register('flight-costs', views.FlightInfoViewSet)
+router.register('flight-info', views.FlightInfoViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
