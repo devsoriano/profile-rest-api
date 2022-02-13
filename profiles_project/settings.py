37180bf5d39a2610ba42096644a97b751857dc43
Rafael Soriano Ramírez <rafael.soriano@arteenarte.com>
2022-02-13 00:17:39 -0600

    add db

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -59,9 +59,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "https://evamed-test.herokuapp.com",
-    "https://evamed.herokuapp.com",
-    "http://localhost:8080",
-    "https://suspicious-hawking-df5be4.netlify.app"
+    "https://evamed.herokuapp.com"
 ]
 
 ROOT_URLCONF = 'profiles_project.urls'
@@ -88,21 +86,13 @@ WSGI_APPLICATION = 'profiles_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-# }
-
-import dj_database_url
-from decouple import config
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -143,5 +133,3 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
