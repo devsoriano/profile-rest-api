952050a8d58ce539facbdc51a59dcfeaa68f264b
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2022-08-24 02:35:41 -0500

    change

--- projects_api/migrations/0038_auto_20210112_0345.py
+++ projects_api/migrations/0038_auto_20210112_0345.py
@@ -4,6 +4,7 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
+    atomic = False  # <<<< THIS LINE
 
     dependencies = [
         ('projects_api', '0037_auto_20210112_0253'),
